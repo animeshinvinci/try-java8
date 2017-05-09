c0ec63efba14584dd9ceba679b3352e4b700a18f
animesh <mishra.animesh.2010@gmail.com>
2017-05-09 20:11:20 +0530

    collcetions streams filter

--- src/FilterList.java
+++ src/FilterList.java
@@ -13,6 +13,7 @@ public class FilterList {
     public static void main(String[] args) {
         List<String> myList =
                 Arrays.asList("a1", "a2", "b1", "c2", "c1");
+
         myList
                 .stream()
                 .filter(s -> s.startsWith("c"))
